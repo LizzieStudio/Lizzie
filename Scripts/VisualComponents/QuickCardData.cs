0c1871979fe3ad7b61b09493d245353e7956f335
Ross-Esmond <r.esmond@protonmail.com>
2026-05-02 08:36:24 -0500

    feat: removed dead files

---
+++ Scripts/VisualComponents/QuickCardData.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+public class QuickCardData
+{
+    public Color BackgroundColor { get; set; }
+    public string Caption { get; set; }
+
+    public Color CardBackColor { get; set; }
+    public string CardBackValue { get; set; }
+}
